647f767592d9616b2faecc56fac81e9f2cee1e90
Tomasz Zebrun <tomasz.zebrun@tron.net.pl>
2022-02-06 14:53:17 +0100

    5.4 Add game logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
-let playerMove = `papier`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyczki, to wygrywasz!');
+console.log('Gracz wybrał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == 1){
+  playerMove = 'kamień';
+} else if(playerInput == 2) {
+  playerMove = `nożyczki`;
+} else if(playerInput == 3) {
+  playerMove = `papier`;
+}
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+} else if(randomNumber == 2) {
+  computerMove = `nożyczki`;
+} else {
+  computerMove = `papier`;
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+printMessage('Twój ruch to: ' + playerMove);
+
+if ((computerMove == 'kamień' && playerMove == 'papier')||
+  (computerMove == 'nożyczki' && playerMove == 'kamień')||
+  (computerMove == 'papier' && playerMove == 'nożyczki')){
+    printMessage('Ty wygrywasz!');
+} else if ((computerMove == 'kamień' && playerMove == 'kamień')||
+  (computerMove == 'nożyczki' && playerMove == 'nożyczki')||       
+  (computerMove == 'papier' && playerMove == 'papier')){
+    printMessage('Remis');
+} else {
+    printMessage('Wygrywa komputer!');
+}
+
